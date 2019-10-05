7d6035b403d95a367208c316c8617e32dd8c1375
seagalputra <seagalputra@gmail.com>
2019-10-05 13:47:46 +0700

    Remove loops and modify using cellfun

--- lib/demo.m
+++ lib/demo.m
@@ -6,14 +6,11 @@ img = imread('../data/1.JPG');
 [obj, gray, out] = imsegment(img);
 
 %% Calculate light source direction for every object
+figure(1);
 imshow(img);
 hold on;
 disp('Estimate light source direction...');
-for i = 1:size(obj,2)
-    L = lightDirection(obj{i}, gray{i});
-    % plot light source direction from center object
-    line([out.center(i,1) out.center(i,1)+L(1)*lenPlot], [out.center(i,2) out.center(i,2)+L(2)*lenPlot], ...
-        'Color', 'red', 'LineWidth', 2);
-end
-% rectangle('Position', [boxProps(i,1), boxProps(i,2), boxProps(i,3), boxProps(i,4)], ...
-%     'EdgeColor', 'r', 'LineWidth', 2);
+L = cellfun(@(obj, gray) lightDirection(obj, gray), obj, gray, 'UniformOutput', false);
+L = cell2mat(L);
+% plot light source direction
+plotLightDirection(img, L, out.center, lenPlot);
