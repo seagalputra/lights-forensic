b679ce5f07350b121fef7baec4c21f72720e7273
seagalputra <seagalputra@gmail.com>
2019-12-08 11:20:17 +0700

    Rename some files

--- lib/addPath.m
+++ lib/addPath.m
@@ -3,6 +3,7 @@ addpath('lights');
 addpath('segmentation');
 addpath('segmentation/meanshift');
 addpath('membership');
+addpath('observationData');
 
 % import mexopencv module for opencv binding
 addpath('C:\dev\mexopencv');
